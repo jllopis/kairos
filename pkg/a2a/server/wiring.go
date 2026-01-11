ef3440ce2b03923a919b8b70aa6f48b0781ab5cb
Joan Llopis <jllopisg@gmail.com>
2026-01-11 23:07:29 +0100

    Wire A2A handler to agent runtime

---
+++ pkg/a2a/server/wiring.go
@@ -0,0 +1,43 @@
+package server
+
+import "github.com/jllopis/kairos/pkg/core"
+
+// HandlerOption customizes the SimpleHandler wiring.
+type HandlerOption func(*SimpleHandler)
+
+// WithStore overrides the task store.
+func WithStore(store TaskStore) HandlerOption {
+	return func(h *SimpleHandler) {
+		if store != nil {
+			h.Store = store
+		}
+	}
+}
+
+// WithExecutor overrides the executor.
+func WithExecutor(exec Executor) HandlerOption {
+	return func(h *SimpleHandler) {
+		if exec != nil {
+			h.Executor = exec
+		}
+	}
+}
+
+// NewAgentHandler wires a SimpleHandler to a Kairos agent.
+func NewAgentHandler(agent core.Agent, opts ...HandlerOption) *SimpleHandler {
+	handler := &SimpleHandler{
+		Store:    NewMemoryTaskStore(),
+		Executor: &AgentExecutor{Agent: agent},
+	}
+	for _, opt := range opts {
+		if opt != nil {
+			opt(handler)
+		}
+	}
+	return handler
+}
+
+// NewAgentService creates a gRPC A2A service wired to a Kairos agent.
+func NewAgentService(agent core.Agent, opts ...HandlerOption) *Service {
+	return New(NewAgentHandler(agent, opts...))
+}
